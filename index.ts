dd6079ae82fb0847815ce6e87585f7bb99adf67d
byungi <tiniwz@gmail.com>
2022-08-16 12:12:26 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import { EffectCallback, useLayoutEffect, useMemo, useRef } from 'react'
 
-export function useEagerEffect(effect: EffectCallback, deps: any[]) {
+export function useEagerEffect(effect: EffectCallback, deps?: any[]) {
     const clearRef = useRef<ReturnType<EffectCallback>>()
-    const prevDepsRef = useRef<any[]>([])
+    const prevDepsRef = useRef<any[]>()
     const prevDeps = prevDepsRef.current
-    if (deps !== prevDeps && (deps.length !== prevDeps.length || prevDeps.some((v, i) => !Object.is(v, deps[i])))) {
+    if (
+        !deps ||
+        !prevDeps ||
+        (deps !== prevDeps && (deps.length !== prevDeps.length || prevDeps.some((v, i) => !Object.is(v, deps[i]))))
+    ) {
         prevDepsRef.current = deps
         clearRef.current?.()
         clearRef.current = effect()
